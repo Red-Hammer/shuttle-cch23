d4d74f2e19be1670c96246eaee67ae553f383ee3
Brandon Sorrow <brandon@offerfit.ai>
2023-12-01 09:55:02 -0500

    First Day Challenge

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use axum::{
+    routing::get, Router,
+    extract::Path,
+};
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct TailPath {
+    tail: String,
+}
+
+async fn packet_recal(Path(TailPath { tail }): Path<TailPath>) -> String {
+
+    let packets: Vec<u32> = tail.split('/').filter_map(|s| s.parse().ok()).collect();
+
+    let mut packet_xor: u32 = packets[0];
+    for packet in &packets[1..] {
+        packet_xor ^= packet;
+    }
+
+    let powed_xor: u32 = packet_xor.pow(3);
+
+    format!("{}", powed_xor)
+}
+
+
+#[shuttle_runtime::main]
+async fn main() -> shuttle_axum::ShuttleAxum {
+    let router = Router::new()
+        .route("/1/*tail", get(packet_recal))
+        ;
+
+    Ok(router.into())
+}
+
